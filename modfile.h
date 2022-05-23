0e6717ceee5ffc56a0574aa53f96d8162bdd6e08
Patrick Cartlidge <patrick.cartlidge@digital.cabinet-office.gov.uk>
2022-05-23 20:04:04 +0100

    big rewrite, pass filedata by ref...

--- modfile.h
+++ modfile.h
@@ -13,34 +13,34 @@ struct file_index_and_length {
   int length;
 };
 
-class Base {
-  public:
-    vector<BYTE> fileData;
-    int get_integer_from_bytes(file_index_and_length z) {
-      // i worked this out last time...
-      // (all_instruments->length[0] << 8 | all_instruments->length[1]) * 2
 
-      // the length needs to be 2 otherwise this will not work
-      // BIG ENDIAN!!!!
-      // MSB is first
-      return ((fileData[z.index] << 8) | fileData[z.index + 1]);
+int get_integer_from_bytes(file_index_and_length z, vector<BYTE>& fileData) {
+  // i worked this out last time...
+  // (all_instruments->length[0] << 8 | all_instruments->length[1]) * 2
 
-    }
-    string get_string_from_bytes(file_index_and_length x) {
-      string string_name;
+  // the length needs to be 2 otherwise this will not work
+  // BIG ENDIAN!!!!
+  // MSB is first
 
-      BYTE* string_name_pointer = &fileData[x.index];
+  cout << ((*(&fileData[z.index]) << 8) | *(&fileData[z.index + 1])) * 2;
 
-      string_name.assign(string_name_pointer, string_name_pointer + x.length);
-      // maybe this should just return the string?
-      // would kind of make sense tbh
-      return string_name;
-    }
-};
+  return ((*(&fileData[z.index]) << 8) | *(&fileData[z.index + 1])) * 2;
 
+}
 
-class Sample: public Base {
+string get_string_from_bytes(file_index_and_length x, vector<BYTE>& fileData) {
+  string string_name;
+
+  string_name.assign(&fileData[x.index], &fileData[x.index] + x.length);
+  // maybe this should just return the string?
+  // would kind of make sense tbh
+
+  return string_name;
+}
+
+class Sample {
   file_index_and_length name, length, finetune, linear_volume, repeat_offset, repeat_length, sample_data;
+  vector<BYTE> *file_data_ref;
 
   public:
     Sample() {};
@@ -51,7 +51,8 @@ class Sample: public Base {
       file_index_and_length *d,
       file_index_and_length *e,
       file_index_and_length *f,
-      vector<BYTE> x
+      file_index_and_length *g,
+      vector<BYTE> *fileData
     ) {
       name = *a;
       length = *b;
@@ -59,33 +60,34 @@ class Sample: public Base {
       linear_volume = *d;
       repeat_offset = *e;
       repeat_length = *f;
-      fileData = x;
+      sample_data = *g;
+      file_data_ref = fileData;
     }
 
     string get_name() {
-      return get_string_from_bytes(name);
+      return get_string_from_bytes(name, *file_data_ref);
     }
     int get_length() {
       // idk why i need 2 multiply the data
-      return get_integer_from_bytes(length) * 2 ;
+      return get_integer_from_bytes(length, *file_data_ref);
     }
 };
 
-class Modfile: public Base {
+class Modfile {
+  vector<BYTE> *fileData;
   file_index_and_length file_name, number_of_patterns, song_end_jump_pos, pattern_table, file_format_tag, pattern_data;
   vector<Sample> sample_bank;
   int total_sample_length;
+  int number_of_samples;
 
   public:
     Modfile(
-      vector<BYTE> a,
-      int fileLengths[5],
+      int fileLengths[6],
       int sampleLengths[6],
-      int number_of_samples
+      int number_of_samples,
+      vector<BYTE> &fd
     ) {
-      sample_bank = vector<Sample>(number_of_samples);
-
-      fileData = a;
+      fileData = &fd;
 
       file_index_and_length file_name, number_of_patterns, song_end_jump_pos, pattern_table, file_format_tag, pattern_data;
       file_index_and_length *file_name_p, *number_of_patterns_p, *song_end_jump_pos_p, *pattern_table_p, *file_format_tag_p, *pattern_data_p;
@@ -97,28 +99,27 @@ class Modfile: public Base {
       file_format_tag_p = &file_format_tag;
       pattern_data_p = &pattern_data;
 
-      // int fileLengths[5] = {
-      //   FILE_NAME_LENGTH,
-      //   FILE_NUMBER_OF_PATTERNS_LENGTH,
-      //   FILE_SONG_END_LENGTH,
-      //   FILE_PATTERN_TABLE_LENGTH,
-      //   FILE_FORMAT_TAG_LENGTH
-      // };
-
       file_name_p->index = 0;
       file_name_p->length = fileLengths[0];
 
-      number_of_patterns_p->index = (number_of_samples * accumulate(sampleLengths, sampleLengths + 6, 0)) + accumulate(fileLengths, fileLengths + 1, 0);
+      // set_sample_bank(sampleLengths, fileLengths[0]);
+
+      int offset_for_file_info = (number_of_samples * accumulate(sampleLengths, sampleLengths + 6, 0));
+
+      number_of_patterns_p->index = offset_for_file_info + accumulate(fileLengths, fileLengths + 1, 0);
       number_of_patterns_p->length = fileLengths[1];
 
-      song_end_jump_pos_p->index = (number_of_samples * accumulate(sampleLengths, sampleLengths + 6, 0)) + accumulate(fileLengths, fileLengths + 2, 0);
+      song_end_jump_pos_p->index = offset_for_file_info + accumulate(fileLengths, fileLengths + 2, 0);
       song_end_jump_pos_p->length = fileLengths[2];
 
-      pattern_table_p->index = (number_of_samples * accumulate(sampleLengths, sampleLengths + 6, 0)) + accumulate(fileLengths, fileLengths + 3, 0);
+      pattern_table_p->index = offset_for_file_info + accumulate(fileLengths, fileLengths + 3, 0);
       pattern_table_p->length = fileLengths[3];
 
-      file_format_tag_p->index = (number_of_samples * accumulate(sampleLengths, sampleLengths + 6, 0)) + accumulate(fileLengths, fileLengths + 4, 0);
+      file_format_tag_p->index = offset_for_file_info + accumulate(fileLengths, fileLengths + 4, 0);
       file_format_tag_p->length = fileLengths[4];
+
+      pattern_data_p->index = offset_for_file_info + accumulate(fileLengths, fileLengths + 5, 0);
+      pattern_data_p->length = fileLengths[5];
 
       set_file_attributes(
         file_name_p,
@@ -128,7 +129,6 @@ class Modfile: public Base {
         file_format_tag_p,
         pattern_data_p
       );
-      set_sample_bank(sampleLengths, fileLengths[0]);
     }
     void set_file_attributes(
       file_index_and_length *a,
@@ -145,75 +145,70 @@ class Modfile: public Base {
       file_format_tag = *e;
       pattern_data = *f;
     }
-    void set_sample_bank(int sampleLengths[6], int lengthOfFileLengths) {
-      // int sampleLengths[6] = {
-      //   SAMPLE_NAME_LENGTH,
-      //   SAMPLE_SAMPLE_LENGTH_LENGTH,
-      //   SAMPLE_FINETUNE_LENGTH,
-      //   SAMPLE_VOLUME_LENGTH,
-      //   SAMPLE_REPEAT_OFFSET_LENGTH,
-      //   SAMPLE_REPEAT_LENGTH
-      // };
-
-      int total_sample_length_acc = 0;
-      int lengthOfSampleLengths = accumulate(sampleLengths, sampleLengths + 6, 0);
-
-      cout << "Hello\n";
-      cout << sample_bank.size();
-      cout << "\n";
-      printf ("%d\n", lengthOfFileLengths);
-
-      for (int i = 0; i < sample_bank.size(); i++) {
-        file_index_and_length name, length, finetune, volume, repeat_offset, repeat_length;
-        file_index_and_length *name_p, *length_p, *finetune_p, *volume_p, *repeat_offset_p, *repeat_length_p;
-
-        // name is of type file_index_and_length
-        // set name_p to point to address at name
-        name_p = &name;
-        length_p = &length;
-        finetune_p = &finetune;
-        volume_p = &volume;
-        repeat_offset_p = &repeat_offset;
-        repeat_length_p = &repeat_length;
-
-
-        // -> in C++ is a function used to assign values to a pointer
-        name_p->index = (lengthOfFileLengths + (lengthOfSampleLengths * i));
-        name_p->length = sampleLengths[0];
-
-        length_p->index = ((lengthOfFileLengths + (lengthOfSampleLengths * i)) + accumulate(sampleLengths, sampleLengths + 1, 0));
-        length_p->length = sampleLengths[1];
-
-        finetune_p->index = ((lengthOfFileLengths + (lengthOfSampleLengths * i)) + accumulate(sampleLengths, sampleLengths + 2, 0));
-        finetune_p->length = sampleLengths[2];
-
-        volume_p->index = ((lengthOfFileLengths + (lengthOfSampleLengths * i)) + accumulate(sampleLengths, sampleLengths + 3, 0));
-        volume_p->length = sampleLengths[3];
-
-        repeat_offset_p->index = ((lengthOfFileLengths + (lengthOfSampleLengths * i)) + accumulate(sampleLengths, sampleLengths + 4, 0));
-        repeat_offset_p->length = sampleLengths[4];
-
-        repeat_length_p->index = ((lengthOfFileLengths + (lengthOfSampleLengths * i)) + accumulate(sampleLengths, sampleLengths + 5, 0));
-        repeat_length_p->length = sampleLengths[5];
-
-        sample_bank[i].set_all(
-          name_p,
-          length_p,
-          finetune_p,
-          volume_p,
-          repeat_offset_p,
-          repeat_length_p,
-          fileData
-        );
-
-        total_sample_length_acc = total_sample_length_acc + sample_bank[i].get_length();
-      }
-      total_sample_length = total_sample_length_acc;
-    }
     string get_name() {
-      return get_string_from_bytes(file_name);
-    }
-    Sample get_sample_bank(int index) {
-      return sample_bank[index];
+      return get_string_from_bytes(file_name, *fileData);
     }
 };
+
+void set_sample_bank(int sampleLengths[6], int lengthOfFileLengths, vector<Sample> & sample_bank, vector<BYTE> & fileData, int number_of_samples) {
+  int total_sample_length_acc = 0;
+  int lengthOfSampleLengths = accumulate(sampleLengths, sampleLengths + 6, 0);
+
+  for (int i = 0; i < number_of_samples; i++) {
+    file_index_and_length name, length, finetune, volume, repeat_offset, repeat_length, file_data;
+    file_index_and_length *name_p, *length_p, *finetune_p, *volume_p, *repeat_offset_p, *repeat_length_p, *file_data_p;
+
+    // name is of type file_index_and_length
+    // set name_p to point to address at name
+    name_p = &name;
+    length_p = &length;
+    finetune_p = &finetune;
+    volume_p = &volume;
+    repeat_offset_p = &repeat_offset;
+    repeat_length_p = &repeat_length;
+    file_data_p = &file_data;
+
+
+    // -> in C++ is a function used to assign values to a pointer
+    name_p->index = (lengthOfFileLengths + (lengthOfSampleLengths * i));
+    name_p->length = sampleLengths[0];
+
+    length_p->index = ((lengthOfFileLengths + (lengthOfSampleLengths * i)) + accumulate(sampleLengths, sampleLengths + 1, 0));
+    length_p->length = sampleLengths[1];
+
+    finetune_p->index = ((lengthOfFileLengths + (lengthOfSampleLengths * i)) + accumulate(sampleLengths, sampleLengths + 2, 0));
+    finetune_p->length = sampleLengths[2];
+
+    volume_p->index = ((lengthOfFileLengths + (lengthOfSampleLengths * i)) + accumulate(sampleLengths, sampleLengths + 3, 0));
+    volume_p->length = sampleLengths[3];
+
+    repeat_offset_p->index = ((lengthOfFileLengths + (lengthOfSampleLengths * i)) + accumulate(sampleLengths, sampleLengths + 4, 0));
+    repeat_offset_p->length = sampleLengths[4];
+
+    repeat_length_p->index = ((lengthOfFileLengths + (lengthOfSampleLengths * i)) + accumulate(sampleLengths, sampleLengths + 5, 0));
+    repeat_length_p->length = sampleLengths[5];
+
+    // calculate offset for samples
+    file_data_p->index = (lengthOfFileLengths + (lengthOfSampleLengths * number_of_samples)) + total_sample_length_acc;
+    file_data_p->length = get_integer_from_bytes(length, fileData) * 2;
+
+    sample_bank[i].set_all(
+      name_p,
+      length_p,
+      finetune_p,
+      volume_p,
+      repeat_offset_p,
+      repeat_length_p,
+      file_data_p,
+      &fileData
+    );
+
+    cout << "\nget sample length\n";
+    printf ("%i\n", total_sample_length_acc);
+    printf ("%d\n", sample_bank[i].get_length());
+
+    sample_bank[i].get_name();
+
+    total_sample_length_acc = total_sample_length_acc + sample_bank[i].get_length();
+  }
+}
--- modplayer.cpp
+++ modplayer.cpp
@@ -1,6 +1,8 @@
-#include <stdio.h>
 #include "modfile.h"
+#include "RtAudio.h"
 
+#include <stdio.h>
+#include <cstdlib>
 #include <string>
 #include <fstream>
 #include <vector>
@@ -20,6 +22,9 @@ const int FILE_NUMBER_OF_PATTERNS_LENGTH = 1;
 const int FILE_SONG_END_LENGTH = 1;
 const int FILE_PATTERN_TABLE_LENGTH = 128;
 const int FILE_FORMAT_TAG_LENGTH = 4;
+const int FILE_PATTERN_DATA_LENGTH = 1024;
+
+const int FILE_PATTERN_LENGTH = 64;
 
 const int SAMPLE_LENGTH = (
   SAMPLE_NAME_LENGTH +
@@ -30,10 +35,57 @@ const int SAMPLE_LENGTH = (
   SAMPLE_REPEAT_LENGTH
 );
 
-const int NUMBER_OF_SAMPLES = 32;
+const int NUMBER_OF_SAMPLES = 31;
+
+// finetune
+// seems to be like subtracting 12 / octave
+
+// period table
+// this is octave 1
+// if you divide by 2 and round to ceil
+// you can get the value of higher octave
+const int period[12] = {
+  1712, //c
+  1616, //c#
+  1524, //d
+  1440, //d#
+  1356, //e
+  1280, //f
+  1208, //f#
+  1140, //g
+  1076, //g#
+  1016, //a
+  960, //a#
+  906 //b
+};
+
+// magic number ! ! ! !
+// PAL:   7093789.2 / (amigaval * 2)
+// to get sample rate
+
 
 typedef unsigned char BYTE;
 using namespace std;
+
+// Two-channel sawtooth wave generator.
+int saw( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+         double streamTime, RtAudioStreamStatus status, void *userData )
+{
+  unsigned int i, j;
+  double *buffer = (double *) outputBuffer;
+  double *lastValues = (double *) userData;
+  if ( status )
+    std::cout << "Stream underflow detected!" << std::endl;
+  // Write interleaved audio data.
+  for ( i=0; i<nBufferFrames; i++ ) {
+    for ( j=0; j<2; j++ ) {
+      *buffer++ = lastValues[j];
+      lastValues[j] += 0.005 * (j+1+(j*0.1));
+      if ( lastValues[j] >= 1.0 ) lastValues[j] -= 2.0;
+    }
+  }
+  return 0;
+}
 
 vector<BYTE> readFile(string filename) {
   // https://stackoverflow.com/questions/15366319/how-to-read-the-binary-file-in-c
@@ -59,12 +111,13 @@ int main() {
   cout << "I am reading the file... \n\n";
 
   vector<BYTE> fileData = readFile(aaa);
-  int fileLengths[5] = {
+  int fileLengths[6] = {
     FILE_NAME_LENGTH,
     FILE_NUMBER_OF_PATTERNS_LENGTH,
     FILE_SONG_END_LENGTH,
     FILE_PATTERN_TABLE_LENGTH,
-    FILE_FORMAT_TAG_LENGTH
+    FILE_FORMAT_TAG_LENGTH,
+    FILE_PATTERN_DATA_LENGTH
   };
   int sampleLengths[6] = {
     SAMPLE_NAME_LENGTH,
@@ -75,11 +128,15 @@ int main() {
     SAMPLE_REPEAT_LENGTH
   };
 
-  Modfile the_file(fileData, fileLengths, sampleLengths, NUMBER_OF_SAMPLES);
+  Modfile the_file(fileLengths, sampleLengths, NUMBER_OF_SAMPLES, fileData);
+  vector<Sample> sample_bank = vector<Sample>(NUMBER_OF_SAMPLES);
+  set_sample_bank(sampleLengths, fileLengths[0], sample_bank, fileData, NUMBER_OF_SAMPLES);
+
+  // number_of_samples = n_o_s;
 
   cout << "File has been read and loaded into object. Sample bank name test...\n";
 
-  Sample sample_bank_example = the_file.get_sample_bank(0);
+  Sample sample_bank_example = sample_bank[0];
 
   cout << sample_bank_example.get_name();
 
@@ -91,5 +148,41 @@ int main() {
 
   cout << sample_bank_example.get_length();
 
+  // http://web.mit.edu/carrien/Public/speechlab/marc_code/ADAPT_VC/rtaudio/doc/html/index.html
+  // https://gist.github.com/nagachika/165241
+
+  RtAudio dac;
+  if ( dac.getDeviceCount() < 1 ) {
+    std::cout << "\nNo audio devices found!\n";
+    exit( 0 );
+  }
+  RtAudio::StreamParameters parameters;
+  parameters.deviceId = dac.getDefaultOutputDevice();
+  parameters.nChannels = 2;
+  parameters.firstChannel = 0;
+  unsigned int sampleRate = 44100;
+  unsigned int bufferFrames = 256; // 256 sample frames
+  double data[2] = {0, 0};
+  try {
+    dac.openStream( &parameters, NULL, RTAUDIO_FLOAT64,
+                    sampleRate, &bufferFrames, &saw, (void *)&data );
+    dac.startStream();
+  }
+  catch ( RtAudioError& e ) {
+    e.printMessage();
+    exit( 0 );
+  }
+
+  char input;
+  std::cout << "\nPlaying ... press <enter> to quit.\n";
+  std::cin.get( input );
+  try {
+    // Stop the stream
+    dac.stopStream();
+  }
+  catch (RtAudioError& e) {
+    e.printMessage();
+  }
+  if ( dac.isStreamOpen() ) dac.closeStream();
   return 0;
 }
